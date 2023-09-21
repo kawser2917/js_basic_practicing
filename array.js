6c0233866966be21626719aad4b4e312b3a0789a
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-22 01:03:03 +0600

    includes and concat

--- array.js
+++ array.js
@@ -11,3 +11,15 @@ console.log(typeof student)
 console.log(typeof friends) // we don't get the type of array with type of funtion
 
 console.log(Array.isArray(friends)) //so we need to use Array.isArray to detect it is array or not
+
+console.log(friends.includes(20)) // checing 20 has in the array or not
+
+// Another way to check a element into array or not 
+if(friends.indexOf(20)!== -1){
+    console.log("21 Is in Frieds")
+}
+
+const newFriends = [5,10,15,18]
+// adding two array element into a single array
+const allFriends = newFriends.concat(friends)
+console.log(allFriends)
